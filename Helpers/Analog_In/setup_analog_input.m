88d41f0bf376ccd44b938ad86048e0f703eb4abe
Austin Pauley <austin2118ace@gmail.com>
2024-12-19 13:22:48 -0500

    Activate second aIn input

--- Helpers/Analog_In/setup_analog_input.m
+++ Helpers/Analog_In/setup_analog_input.m
@@ -6,11 +6,18 @@ function a_in = setup_analog_input(COM)
         error('Error connecting to analog input on COM: %s', string(COM));
     end
 
-    a_in.InputRange(1) = {"0V:10V"};
-    a_in.nActiveChannels = 1;
+    a_in.nActiveChannels = 2;
     a_in.Thresholds(1) = 10;
+    a_in.InputRange(1) = {"0V:10V"};
     a_in.ResetVoltages(1) = 9.95;
     a_in.SMeventsEnabled(1) = 1;
     a_in.Stream2USB(1) = 1;
+
+    a_in.Thresholds(2) = 10;
+    a_in.InputRange(2) = {"0V:10V"};
+    a_in.ResetVoltages(2) = 9.95;
+    a_in.SMeventsEnabled(2) = 1;
+    a_in.Stream2USB(2) = 1;
+
 
 end
